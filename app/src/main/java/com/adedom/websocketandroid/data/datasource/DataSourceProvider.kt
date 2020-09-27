3f2d00885e18da43cb6f269de2935593ce1f21d3
AdeDom <pathiphonyw@gmail.com>
2020-09-27 19:21:02 +0700

    Delete library retrofit and replace Ktor HttpClient

--- app/src/main/java/com/adedom/websocketandroid/data/datasource/DataSourceProvider.kt
+++ app/src/main/java/com/adedom/websocketandroid/data/datasource/DataSourceProvider.kt
@@ -1,21 +1,12 @@
 package com.adedom.websocketandroid.data.datasource
 
-import com.adedom.websocketandroid.data.network.DruChatWebSocket
 import com.adedom.websocketandroid.data.network.ApiService
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.adedom.websocketandroid.data.network.DruChatWebSocket
 
 class DataSourceProvider {
 
     fun getProviderDruChatWebSocket(): DruChatWebSocket = DruChatWebSocket()
 
-    fun getProviderApiService(): ApiService {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://adedom-chatv2.herokuapp.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        return retrofit.create(ApiService::class.java)
-    }
+    fun getProviderApiService() = ApiService()
 
 }
--- app/src/main/java/com/adedom/websocketandroid/data/network/ApiService.kt
+++ app/src/main/java/com/adedom/websocketandroid/data/network/ApiService.kt
@@ -1,11 +1,35 @@
 package com.adedom.websocketandroid.data.network
 
 import com.chat.FetchChatResponse
-import retrofit2.http.GET
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
 
-interface ApiService {
+class ApiService {
 
-    @GET("api/fetch-chat")
-    suspend fun fetchChat(): FetchChatResponse
+    private val client = HttpClient(OkHttp) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60_000
+        }
+
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+    }
+
+    suspend fun fetchChat(): FetchChatResponse {
+        return client.get("${BASE_URL}api/fetch-chat")
+    }
+
+    companion object {
+        private const val BASE_URL = "https://adedom-chatv2.herokuapp.com/"
+    }
 
 }
